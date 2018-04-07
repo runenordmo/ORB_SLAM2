e8726b63a7d7366932e40a6889f97670ba845d1b
runenordmo <rune.nordmo@gmail.com>
2018-04-07 19:55:13 +0200

    Added temporary SURFextractor, to be tested with visualization

---
+++ include/SURFextractor.h
@@ -0,0 +1,49 @@
+//
+// Created by rune on 07.04.18.
+//
+
+#ifndef ORB_SLAM2_SURFEXTRACTOR_H
+#define ORB_SLAM2_SURFEXTRACTOR_H
+
+#include <vector>
+#include <list>
+#include <opencv/cv.h>
+
+
+namespace ORB_SLAM2
+{
+
+class SURFextractor {
+public:
+
+    //ORBextractor(int nfeatures, float scaleFactor, int nlevels,
+    //            int iniThFAST, int minThFAST);
+    // NOT SURE HOW TO HANDLE THE SCALE WITH THE SURF EXTRACTOR
+    SURFextractor(int nfeatures, float scaleFactor, int nlevels);
+
+    ~SURFextractor(){}
+
+    // Compute the SURF features and descriptors on an image.
+    // Mask is ignored in the current implementation.
+    void operator()( cv::InputArray image, cv::InputArray mask,
+                     std::vector<cv::KeyPoint>& keypoints,
+                     cv::OutputArray descriptors);
+
+protected:
+
+    // USE OCTTREE HERE?
+
+    int nfeatures;
+
+    cv::Ptr<cv::Feature2D> mSurfDetector;
+
+    // USE SCALEFACTOR AND NLEVELS HERE?
+
+};
+
+
+
+} //namespace ORB_SLAM
+
+
+#endif //ORB_SLAM2_SURFEXTRACTOR_H
---
+++ src/SURFextractor.cc
@@ -0,0 +1,34 @@
+//
+// Created by rune on 07.04.18.
+//
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/features2d/features2d.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <vector>
+
+#include "SURFextractor.h"
+
+// SURF SPECIFIC:
+
+#include "opencv2/features2d.hpp"
+#include "opencv2/xfeatures2d.hpp"
+
+using namespace cv;
+using namespace std;
+
+namespace ORB_SLAM2
+{
+
+SURFextractor::SURFextractor(int nfeatures, float scaleFactor, int nlevels) {
+    double hessian = 800.0;
+    mSurfDetector = xfeatures2d::SURF::create();
+}
+
+void SURFextractor::operator()(const cv::_InputArray &image, const cv::_InputArray &mask,
+                                          std::vector<cv::KeyPoint> &keypoints, const cv::_OutputArray &descriptors) {
+    mSurfDetector->detectAndCompute(image, mask, keypoints, descriptors, false);
+}
+
+} //namespace ORB_SLAM
